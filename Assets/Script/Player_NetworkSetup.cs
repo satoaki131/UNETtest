503caf71e0bdbf219027e37bfd234e40139c5b80
Satoaki <akinori_0131@yahoo.co.jp>
2015-12-14 15:40:07 +0900

    Player座標同期

---
+++ Assets/Script/Player_NetworkSetup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Camera FPSCharacterCam;
+    [SerializeField]
+    AudioListener audioListener;
+
+
+	void Start ()
+    {
+	    if(isLocalPlayer)
+        {
+            GameObject.Find("SceneCamera").SetActive(false);
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+            FPSCharacterCam.enabled = true;
+            audioListener.enabled = true;
+        }
+	}
+	
+	
+}
